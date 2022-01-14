2d4a354a8f673f1cda5f20985c6bc3593fa07071
BIT <BIT@DESKTOP-BM40P2C>
2022-01-14 15:42:18 +0900

    mysite02 #mvcutil

---
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/user/UserActionFactory.java
@@ -0,0 +1,10 @@
+package com.poscoict.mysite.mvc.user;
+
+import com.poscoict.web.mvc.Action;
+
+public class UserActionFactory {
+	public Action getAction(String actionName) {
+		if()
+		return null;
+	}
+}
